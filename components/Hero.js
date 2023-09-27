e8d25decfc1cb846bfb6c84420e20212cad658d6
Tomas Gonzalez <tomimateo2008@hotmail.com>
2023-09-27 17:36:21 -0300

    actualizacion

---
+++ components/Hero.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+import { RainbowHighlight } from "./RainbowHighlight";
+import userData from "@constants/data";
+
+export default function Hero() {
+  const colors = ["#008EA2"];
+  return (
+    <div className=" justify-center items-start overflow-hidden h-screen flex flex-col">
+      <div className="w-full md:w-1/2 mx-auto text-center md:text-left lg:p-20">
+        <RoughNotationGroup show={true}>
+          <RainbowHighlight color={colors[0]}>
+            <h1 className="text-4xl md:text-8xl font-bold text-black-700 dark:text-gray-200 my-2">
+              Tomas.
+            </h1>
+          </RainbowHighlight>
+
+          <RainbowHighlight color={colors[0]}>
+            <h1 className="text-4xl md:text-8xl font-bold text-black-700 dark:text-gray-200 my-2">
+              Gonzalez.
+            </h1>
+          </RainbowHighlight>
+        </RoughNotationGroup>
+      </div>
+      {/* Image container */}
+      <div className="hidden lg:block relative w-full ">
+        <div className="w-3/4   relative bottom-20 left-20">
+          {/* <img src={userData.avatarUrl} alt="avatar" className=" shadow" /> */}
+          <div className="flex flex-row justify-between mt-4 ">
+            <div className="flex flex-row space-x-4 relative left-2/3 ">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                className="bi bi-arrow-90deg-up"
+                viewBox="0 0 16 16"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.854 1.146a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L4 2.707V12.5A2.5 2.5 0 0 0 6.5 15h8a.5.5 0 0 0 0-1h-8A1.5 1.5 0 0 1 5 12.5V2.707l3.146 3.147a.5.5 0 1 0 .708-.708l-4-4z"
+                />
+              </svg>
+              <p className="font-mono ">That's me</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ constants/data.js
@@ -0,0 +1,114 @@
+const userData = {
+  githubUsername: "tomigonzalez",
+  name: "Tomas Gonzalez",
+  designation: "Full-Stack Developer",
+
+  email: "tomigonnzalez1@gmail.com",
+  phone: "+54 2915164198",
+  address: "Bahía Blanca Provincia de Buenos Aires, Argentina.",
+  projects: [
+    {
+      title: "Shoes E-commerce",
+      link: "https://js-shoes.vercel.app/",
+      imgUrl: "/Shoes.jpg",
+    },
+    {
+      title: "Casino Full-stack",
+      link: "https://casino-frontend-two.vercel.app/",
+      imgUrl: "/casino.jpg",
+    },
+    {
+      title: "Casino Firebase-React",
+      link: "https://casino-aa23a.web.app/",
+      imgUrl: "/casino.jpg",
+    },
+    {
+      title: "Pizzas E-commerce",
+      link: "https://pizzas-e-comerce.vercel.app/",
+      imgUrl: "/Pizzas.jpg",
+    },
+    {
+      title: "Casino back-end",
+      link: "https://express.app.render.com/",
+      imgUrl: "/node4.jpg",
+    },
+    {
+      title: "BackendGastos-Prisma-SQL-TS-Express",
+      link: "https://github.com/tomigonzalez/BackendGastos-Prisma-SQL-TS-Express",
+      imgUrl: "/SQL.png",
+    },
+    {
+      title: "ToDo React",
+      link: "https://todolistreact-five.vercel.app/",
+      imgUrl: "/Todo.jpg",
+    },
+    {
+      title: "Project HTML-CSS",
+      link: "https://projectofinal.vercel.app/",
+      imgUrl: "/9z.jpg",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer who loves creating beautiful products and websites.",
+    description: [
+      "Hello, I m Tomás González! At 23 years old, I am a passionate web developer specialized in Front-end with React. My journey in the world of programming began as a self-taught person, and since then, I have been in constant search for knowledge and challenges.",
+
+      "In addition to my Front-end experience, I am exploring the world of Back-end, where I use technologies such as Node.js, SQL and MongoDB to create robust and scalable solutions. This diversity of skills allows me to tackle projects from user interface to server logic.",
+
+      "My passion for learning led me to complete specialized bootcamps and I am currently pursuing a university degree to continue deepening my knowledge. I love facing new challenges and learning new technologies to stay up to date in a constantly evolving field.",
+
+      "But my interest is not limited only to code. I also like design and user experience (UX/UI). I enjoy creating designs for my projects using tools like Photoshop and Figma. This combination of technical and creative skills allows me to offer complete and attractive solutions to my clients.",
+
+      "My goal is to continue growing as a professional, facing exciting challenges and collaborating with passionate people. Through my portfolio, I hope to share my journey, my achievements, and my passion for technology and design.",
+    ],
+    currentProject: "Casino Full-stack",
+    currentProjectUrl: "https://casino-frontend-two.vercel.app/",
+  },
+  experience: [
+    {
+      title: "Ingenieria en sistemas de informacion",
+      company: "Universidad nacional del sur",
+      year: "2021-Actualmente",
+      companyLink: "https://www.uns.edu.ar/",
+      desc: "",
+    },
+    {
+      title: "Freelance",
+      company: "Desarrollador",
+      year: "2021-Actualmente ",
+      companyLink: "",
+      desc: "Actualmente estoy implementando proyectos individuales como grupales para continuar con mi formacion.",
+    },
+    {
+      title: "Diseño web responsive",
+      company: "UTN",
+      year: "2021-2022",
+      companyLink: "https://utn.edu.ar/es/",
+      desc: "Desarrollo de paginas web con html , css y frameworks responsive. ",
+    },
+    {
+      title: "Desarrollador Full-stack",
+      company: "Nucba",
+      year: "2021-2023",
+      companyLink: "https://www.nucba.com.ar/",
+      desc: "Desarrollador web, del lado del usuario y del servidor.",
+    },
+    {
+      title: "Desarrollo Full-stack",
+      company: "Argentina programa",
+      year: "2022-actualmente",
+      companyLink:
+        "https://www.argentina.gob.ar/economia/conocimiento/argentina-programa/programacion",
+      desc: "Desarrollador web, esta vez usando tecnologias como PHP y frameworks como laravel.",
+    },
+  ],
+  resumeUrl: "",
+  socialLinks: {
+    instagram: "https://www.instagram.com/tomigonnzalez1/?hl=es",
+    linkedin: "https://www.linkedin.com/in/tomigonzalez",
+    github: "https://github.com/tomigonzalez",
+  },
+};
+
+export default userData;
